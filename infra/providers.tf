c399e9500caf6330215f8cfcd51f05cb2de62e56
zzermani <zzermani@novomind.com>
2024-06-30 18:08:24 +0200

    remettre provider part

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,12 +1,12 @@
 # Setting and locking the Dependencies
 terraform {
-  # required_providers {
-  #   aws = {
-  #     source  = "hashicorp/aws"
-  #     version = "4.2"
-  #   }
-  # }
-  # Setting the Terraform version
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.2"
+    }
+  }
+  #Setting the Terraform version
   required_version = ">= 1.1.0"
 }
 
