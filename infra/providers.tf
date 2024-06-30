8baef62731e61bbd48fd509667890f237193dda8
zzermani <zzermani@novomind.com>
2024-06-30 18:03:42 +0200

    delete provider part

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,11 +1,11 @@
 # Setting and locking the Dependencies
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.2"
-    }
-  }
+  # required_providers {
+  #   aws = {
+  #     source  = "hashicorp/aws"
+  #     version = "4.2"
+  #   }
+  # }
   # Setting the Terraform version
   required_version = ">= 1.1.0"
 }
